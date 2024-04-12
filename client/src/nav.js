8e1ffb0b1475e30e0b0ee04da7f62cb30bab378e
Tewodros Assefa <teddyassef23@gmail.com>
2024-04-12 07:57:58 -0500

    Member Layout page is done

---
+++ client/src/nav.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import Nav from './nav/index';
+
+const root = ReactDOM.createRoot(document.getElementById("nav"));
+root.render(
+    <Nav />
+);
