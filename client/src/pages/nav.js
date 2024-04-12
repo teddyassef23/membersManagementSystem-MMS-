8d62c3e75893055127e2909e6d4de53b3d000ff2
teddyassef23 <145693420+teddyassef23@users.noreply.github.com>
2024-04-12 08:15:18 -0500

    Delete client/src/pages directory

--- client/src/pages/nav.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
-import Nav from './nav/index';
-
-const root = ReactDOM.createRoot(document.getElementById("nav"));
-root.render(
-    <Nav />
-);
