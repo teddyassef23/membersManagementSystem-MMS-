2d4a6dae34d19111a473355ae388fd347c5b7f6f
teddyassef23 <145693420+teddyassef23@users.noreply.github.com>
2024-04-16 20:00:31 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,3 +18,45 @@ db.once('open', () => {
     console.log(`API serve  running on port ${PORT}!`);
   });
 });
+
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth'); 
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+async function startServer() {
+  const app = express();
+
+  app.use(express.json()); 
+  app.use(express.urlencoded({ extended: true })); 
+  app.use(cors()); 
+
+//   await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/members', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: ({ req }) => ({ user: req.user }), 
+//   });
+
+//   app.use(authMiddleware);
+
+//   await server.start();
+
+//   server.applyMiddleware({ app });
+
+//   const PORT = process.env.PORT || 3001;
+
+//   app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// }
+
+startServer().catch(err => {
+  console.error('Error starting server:', err);
+});
